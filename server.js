73694ca27a982dda491bae5e505a052dff9837ca
Артем Аверьянов <artemaveranov@Averyanovs-MacBook-Pro.local>
2022-07-24 13:33:36 +0300

    fix

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 4333;
 
 const items = [{id: 1, checked: false, text: 'Buy beer'}, {id: 2, checked: false, text: 'Buy milk'}];
 
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
+
 app.get('/list', (req, res) => {
     try {
         res.json(items);
